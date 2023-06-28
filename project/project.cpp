acd6a8748fcd93ade50359bce888b270956d8489
Bob <bob@example.com>
2023-06-28 21:48:45 +0300

    code: merge

--- project/project.cpp
+++ project/project.cpp
@@ -11,9 +11,6 @@ int main()
         << "A * B = " << a * b << '\n'
         << "A / B = " << a / b << '\n';
 
-<<<<<<< HEAD
     a > b ? cout << a << '\n' : cout << b << '\n';
-=======
     a < b ? cout << a << '\n' : cout << b << '\n';
->>>>>>> 779d8ed (code: add output min)
 }
