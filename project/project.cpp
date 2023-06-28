01f1f5383edfd9b8b2d876cbd8dec8dea82ac06a
Bob <bob@example.com>
2023-06-28 21:45:46 +0300

    code: some changes

--- project/project.cpp
+++ project/project.cpp
@@ -11,5 +11,9 @@ int main()
         << "A * B = " << a * b << '\n'
         << "A / B = " << a / b << '\n';
 
+<<<<<<< HEAD
     a > b ? cout << a << '\n' : cout << b << '\n';
+=======
+    a < b ? cout << a << '\n' : cout << b << '\n';
+>>>>>>> 779d8ed (code: add output min)
 }
